581220187729cf63da7f40c4f872f5ddd1a29649
정회석 <jsk2343@naver.com>
2025-01-19 13:10:36 +0900

    init: init

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from 'next';
+import { Noto_Sans_KR } from 'next/font/google';
+
+import { cn } from '@/lib/styleUtils';
+
+const notoSansKR = Noto_Sans_KR({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-noto-sans-kr',
+});
+
+export const metadata: Metadata = {
+  title: 'NextJs Boilerplate',
+  description: 'nextjs Boilerplate setting',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='ko' className={cn(notoSansKR.variable)}>
+      <body>{children}</body>
+    </html>
+  );
+}
